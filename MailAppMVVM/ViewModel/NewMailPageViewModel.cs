b22f69a03f0bb8b8b5119bab6f04757f669e8bce
juan2305 <78822907+juan2305@users.noreply.github.com>
2021-09-01 19:23:12 -0400

    Implementing Data Binding, but just a Sample in Main Page

---
+++ MailAppMVVM/ViewModel/NewMailPageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MailAppMVVM.ViewModel
+{
+   public class NewMailPageViewModel
+    {
+        public string[] MailList { get; set; }
+
+       public NewMailPageViewModel()
+        {
+            MailList = new string[] { "From: John", "From: Mayelin", "From: Juan" };
+        }
+    }
+}
--- MailAppMVVM/Views/MainPage.xaml.cs
+++ MailAppMVVM/Views/MainPage.xaml.cs
@@ -13,6 +13,7 @@ namespace MailAppMVVM
         public MainPage()
         {
             InitializeComponent();
+            
         }
     }
 }
